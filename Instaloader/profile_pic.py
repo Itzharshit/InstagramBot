6eaa2b8941357b4688e99c44b89cb129050c985d
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-12-16 10:16:05 +0530

    Update profile_pic.py

--- Instaloader/profile_pic.py
+++ Instaloader/profile_pic.py
@@ -9,11 +9,11 @@ from instaloader.exceptions import QueryReturnedNotFoundException, ProfileNotExi
 async def dp(_, msg):
     status = await msg.reply('Please Wait...', quote=True)
     if len(msg.command) == 1:
-        await msg.reply("Please do not use empty command. Below is the right format to get a profile pic. "
+        await msg.reply("Oops! Please use perfect command. Below is the right format to get a profile pic. "
                         "\n\n`/profile_pic instagram-username` \n\nExample : `/profile_pic taaarannn`")
         return
     elif len(msg.command) > 2:
-        await msg.reply("Use 1 username at a time.")
+        await msg.reply("Use one username at a time.")
         return
     text = msg.command[1]
     if text.startswith('@'):
@@ -27,7 +27,7 @@ async def dp(_, msg):
     files = os.listdir(text.lower())
     for file in files:
         if file.endswith(".jpg"):
-            caption = f"Profile Picture of [@{text}](https://instagram.com/{text}) \n\nBy @StarkBots"
+            caption = f"Profile Picture of [@{text}](https://instagram.com/{text})"
             await msg.reply_photo(f"{text}/{file}", caption=caption)
             await status.delete()
     shutil.rmtree(text)
